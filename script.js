ba93cffa733f6c450e4283ddf0cfac852b28a076
Ms-Freddy <74541505+Ms-Freddy@users.noreply.github.com>
2021-06-01 05:07:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,29 @@
 
 //console.log(result);
 
-const user = "Andrew";
 
-alert(`Привет, ${user}`);
+
+const numberOfFilms =prompt("Сколько фильмов вы уже посмотрели ?", "");
+
+const personalMovieDB = {
+    count:numberOfFilms,
+    movies:{},
+    actors:{},
+    genres:[],
+    privat:false
+};
+
+const a =prompt("Один из просмотренных фильмов ?", "");
+const b =prompt("На сколько оцените его?", "") ;
+const c =prompt("Один из просмотренных фильмов ?", "");
+const d =prompt("На сколько оцените его?", "") ;
+
+personalMovieDB.movies[a] = b ;
+personalMovieDB.movies[c] = d ;
+
+console.log(personalMovieDB);
+
+
+
+
+
